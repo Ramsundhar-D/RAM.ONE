f2cef6b09e7c97202f34425a8abf0a3da1f39883
Ramsundhar-D <ramsundhardrs@gmail.com>
2022-09-05 20:27:46 +0530

    my third update

---
+++ java/id.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class id{
+
+public static void main(String args [])
+
+{
+
+Scanner x = new Scanner (System.in);
+
+System.out.println("Enter the Mark");
+
+int mark=x.nextInt();
+
+if(mark>=30)
+{
+
+System.out.println("Pass");
+}
+else
+{
+System.out.println("fail");
+}
+
+
+
+}
+}
+
